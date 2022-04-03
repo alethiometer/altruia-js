4a42948375221bca791a74fbe80674c68c0c0af4
alethiometer <sofiawawrzyniak@gmail.com>
2022-04-03 12:02:11 -0400

    charity nfts

--- src/pages/Charity.jsx
+++ src/pages/Charity.jsx
@@ -11,6 +11,31 @@ import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
+import woodBronze from './wood-bronze.png';
+import woodDiamond from './wood-diamond.png';
+import woodGold from './wood-gold.png';
+import woodSilver from './wood-silver.png';
+import woodWood from './wood-wood.png';
+import bronzeBronze from './bronze-bronze.png';
+import bronzeDiamond from './bronze-diamond.png';
+import bronzeGold from './bronze-gold.png';
+import bronzeSilver from './bronze-silver.png';
+import bronzeWood from './bronze-wood.png';
+import silverBronze from './silver-bronze.png';
+import silverDiamond from './silver-diamond.png';
+import silverGold from './silver-gold.png';
+import silverSilver from './silver-silver.png';
+import silverWood from './silver-wood.png';
+import goldBronze from './gold-bronze.png';
+import goldDiamond from './gold-diamond .png';
+import goldGold from './gold-gold.png';
+import goldSilver from './gold-silver.png';
+import goldWood from './gold-wood.png';
+import diamondBronze from './diamond-bronze.png';
+import diamondDiamond from './diamond-diamond.png';
+import diamondGold from './diamond-gold.png';
+import diamondSilver from './diamond-silver.png';
+import diamondWood from './diamond-wood.png';
 
 import Web3 from "web3";
 import donations from "../donations";
@@ -624,6 +649,35 @@ function Charity() {
                     <p>Top donors to the Ukrainian Crisis</p>
                 </div>
                 <img src={leaderboard} className="leaderboard-img" />
+            </div>
+            <h4 className="gallery-header">Tokens of appreciation</h4>
+                <p className="gallery-header">The set of NFTs that represent your contribution to the Ukrainian Crisis.</p>
+            <div className="NFT-grid">
+                <img src={diamondDiamond} className="NFT"/>
+                <img src={diamondGold} className="NFT"/>
+                <img src={diamondSilver} className="NFT"/>
+                <img src={diamondBronze} className="NFT"/>
+                <img src={diamondWood} className="NFT"/>
+                <img src={goldDiamond} className="NFT"/>
+                <img src={goldGold} className="NFT"/>
+                <img src={goldSilver} className="NFT"/>
+                <img src={goldBronze} className="NFT"/>
+                <img src={goldWood} className="NFT"/>
+                <img src={silverDiamond} className="NFT"/>
+                <img src={silverGold} className="NFT"/>
+                <img src={silverSilver} className="NFT"/>
+                <img src={silverBronze} className="NFT"/>
+                <img src={silverWood} className="NFT"/>
+                <img src={bronzeDiamond} className="NFT"/>
+                <img src={bronzeGold} className="NFT"/>
+                <img src={bronzeSilver} className="NFT"/>
+                <img src={bronzeBronze} className="NFT"/>
+                <img src={bronzeWood} className="NFT"/>
+                <img src={woodDiamond} className="NFT"/>
+                <img src={woodGold} className="NFT"/>
+                <img src={woodSilver} className="NFT"/>
+                <img src={woodBronze} className="NFT"/>
+                <img src={woodWood} className="NFT"/>
             </div>
 
             <Modal show={showDonate} onHide={handleCloseDonate} className="donateModal" size="lg">
