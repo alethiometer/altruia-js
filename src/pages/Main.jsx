4c64aa08bad754d9d0b0fff5f07b14735ff7d6a3
alethiometer <sofiawawrzyniak@gmail.com>
2022-04-03 06:54:39 -0400

    landing page updates

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import "./Main.css";
-import leaderboard from './leaderboard-overall.png'
+import leaderboard from './leaderboard-overall.png';
+import woodBronze from './wood-bronze.png';
+import woodDiamond from './wood-diamond.png';
+import woodGold from './wood-gold.png';
+import woodSilver from './wood-silver.png';
+import woodWood from './wood-wood.png';
+import bronzeBronze from './bronze-bronze.png';
+import bronzeDiamond from './bronze-diamond.png';
+import bronzeGold from './bronze-gold.png';
+import bronzeSilver from './bronze-silver.png';
+import bronzeWood from './bronze-wood.png';
+import silverBronze from './silver-bronze.png';
+import silverDiamond from './silver-diamond.png';
+import silverGold from './silver-gold.png';
+import silverSilver from './silver-silver.png';
+import silverWood from './silver-wood.png';
+import goldBronze from './gold-bronze.png';
+import goldDiamond from './gold-diamond .png';
+import goldGold from './gold-gold.png';
+import goldSilver from './gold-silver.png';
+import goldWood from './gold-wood.png';
+import diamondBronze from './diamond-bronze.png';
+import diamondDiamond from './diamond-diamond.png';
+import diamondGold from './diamond-gold.png';
+import diamondSilver from './diamond-silver.png';
+import diamondWood from './diamond-wood.png';
+
 
 function Main() {
     return (
@@ -44,8 +70,34 @@ function Main() {
                 <p>The top givers, globally.</p>
                 <img src={leaderboard} className="leaderboard-img"/>
             </div>
+            <h4>Tokens of appreciation</h4>
+                <p>The set of NFTs that represent your contribution to the Ukrainian Crisis.</p>
             <div className="NFT-grid">
-
+                <img src={diamondDiamond} className="NFT"/>
+                <img src={diamondGold} className="NFT"/>
+                <img src={diamondSilver} className="NFT"/>
+                <img src={diamondBronze} className="NFT"/>
+                <img src={diamondWood} className="NFT"/>
+                <img src={goldDiamond} className="NFT"/>
+                <img src={goldGold} className="NFT"/>
+                <img src={goldSilver} className="NFT"/>
+                <img src={goldBronze} className="NFT"/>
+                <img src={goldWood} className="NFT"/>
+                <img src={silverDiamond} className="NFT"/>
+                <img src={silverGold} className="NFT"/>
+                <img src={silverSilver} className="NFT"/>
+                <img src={silverBronze} className="NFT"/>
+                <img src={silverWood} className="NFT"/>
+                <img src={bronzeDiamond} className="NFT"/>
+                <img src={bronzeGold} className="NFT"/>
+                <img src={bronzeSilver} className="NFT"/>
+                <img src={bronzeBronze} className="NFT"/>
+                <img src={bronzeWood} className="NFT"/>
+                <img src={woodDiamond} className="NFT"/>
+                <img src={woodGold} className="NFT"/>
+                <img src={woodSilver} className="NFT"/>
+                <img src={woodBronze} className="NFT"/>
+                <img src={woodWood} className="NFT"/>
             </div>
         </div>
     );
