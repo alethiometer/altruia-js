ae22a83dec1e92874c0b50096a528f307e81da89
alethiometer <sofiawawrzyniak@gmail.com>
2022-04-02 22:56:49 -0400

    wallet connect functionality

--- src/pages/Charity.jsx
+++ src/pages/Charity.jsx
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import "./Charity.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-// Web3
+import web3 from "../web3";
+import donations from "../donations";
 
 function Charity() {
-    const [showDonate, setShowDonate] = useState(false);
+    const [account, setAccount] = useState(null);
 
+    const checkWalletIsConnected = () => { 
+        const { ethereum } = window;
+
+        if (!ethereum) {
+            console.log("Make sure you have Metamask installed!");
+            return;
+        } else {
+            console.log("Wallet exists! We're ready to go!")
+        }
+    }
+    const connectWalletHandler = async () => {
+        const { ethereum } = window;
+
+        if (!ethereum) {
+            alert("Please install Metamask!");
+        }
+
+        try {
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            setAccount(accounts[0]);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        checkWalletIsConnected();
+      }, [])
+
+    // Donate modal
+    const [showDonate, setShowDonate] = useState(false);
     const handleCloseDonate = () => setShowDonate(false);
     const handleShowDonate = () => setShowDonate(true);
 
+    // Raise modal
     const [showRaise, setShowRaise] = useState(false);
-
     const handleCloseRaise = () => setShowRaise(false);
     const handleShowRaise = () => setShowRaise(true);
 
     return (
         <div className="container">
             <div className="wallet-connect">
-                <Button className="wallet">Connect wallet</Button>
+                {account ? <Button className="wallet-connected">Wallet connected</Button>
+                : <Button onClick={connectWalletHandler} className="wallet-not-connected">Connect wallet</Button>}
             </div>
             <div className="header">
                 <h2>$1,000,000 raised</h2>
